5aaa592bfe034ec2086f1e8960e455984a780fce
sirjan07 <sirjankharel55@gmail.com>
2026-09-23 19:34:44 +0545

    Add files via upload

---
+++ ascending.c.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+	int i,j,a[5],k;
+	printf("enter any number");
+	for (i=0;i<5;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for (i=0;i<5;i++)
+	{
+		for(j=0;j<5;j++)
+		{
+			if (a[j]>a[j+1])
+			{
+				k=a[j];
+				a[j]=a[j+1];
+				a[j+1]=k;
+			}
+		}
+	}
+	for(i=0;i<5;i++)
+	{
+		printf("%d" ,a[i]);
+	}
+	printf("\n");
+	return(0);
+}
